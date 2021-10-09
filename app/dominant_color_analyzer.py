f3d872cc99669ef6d6eda649b3b3329308b072cd
ferdyferdy9 <ferdyferdy9514@gmail.com>
2021-10-09 14:36:44 +0700

    Add Analyze path to DominantColorAnalyzer

--- app/dominant_color_analyzer.py
+++ app/dominant_color_analyzer.py
@@ -1,4 +1,5 @@
 import numpy
+import cv2
 
 from sklearn.cluster import KMeans
 
@@ -9,7 +10,12 @@ class DominantColorAnalyzer:
         self.image_cluster_index = None
         self.dominant_colors = None
 
-    def analyze(self, image_data):
+    def analyze_path(self, image_path):
+        image = cv2.imread(image_path)
+        hsvImage = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        self.analyze_image(hsvImage)
+
+    def analyze_image(self, image_data):
         km = KMeans(n_clusters=self.total_cluster)
         self.image_cluster_index = km.fit_predict(image_data)
         self.dominant_colors = km.cluster_centers_
--- test/test_dominant_color_analyzer.py
+++ test/test_dominant_color_analyzer.py
@@ -8,30 +8,50 @@ class TestDominantColorAnalyzer(unittest.TestCase):
     def setUp(self):
         self._color_analyzer = DominantColorAnalyzer()
 
+    @patch('app.dominant_color_analyzer.cv2')
+    def test_analyze_path(self, mock_cv2):
+        TEST_IMAGE_PATH = "image_path"
+        EXPECTED_IMAGE_RESULT = "image_result"
+        EXPECTED_HSV_RESULT = "hsv_result"
+
+        mock_cv2.COLOR_BGR2HSV = -1
+        mock_cv2.imread.return_value = EXPECTED_IMAGE_RESULT
+        mock_cv2.cvtColor.return_value = EXPECTED_HSV_RESULT
+
+        with patch.object(self._color_analyzer, "analyze_image") as mock_analyze_image:
+            self._color_analyzer.analyze_path(TEST_IMAGE_PATH)
+
+            mock_cv2.imread.assert_called_with(TEST_IMAGE_PATH)
+            mock_cv2.cvtColor.assert_called_with(EXPECTED_IMAGE_RESULT, -1)
+            mock_analyze_image.assert_called_with(EXPECTED_HSV_RESULT)
+
     @patch('app.dominant_color_analyzer.KMeans')
-    def test_analyze(self, mock_color_analyzer):
+    def test_analyze_image(self, mock_kmeans):
         TEST_IMAGE_DATA = [[5, 6, 7]]
         EXPECTED_CLUSTER_INDEX = [3, 1, 0, 2]
         EXPECTED_DOMINANT_COLORS = [[0,1,2], [1,2,3]]
 
-        instance = mock_color_analyzer.return_value
+        instance = mock_kmeans.return_value
         instance.fit_predict.return_value = EXPECTED_CLUSTER_INDEX
         instance.cluster_centers_ = EXPECTED_DOMINANT_COLORS
 
-        self._color_analyzer.analyze(TEST_IMAGE_DATA)
+        self._color_analyzer.analyze_image(TEST_IMAGE_DATA)
 
         self.assertEqual(EXPECTED_CLUSTER_INDEX, self._color_analyzer.image_cluster_index)
         self.assertEqual(EXPECTED_DOMINANT_COLORS, self._color_analyzer.dominant_colors)
 
-
     def test_get_top_5_colors(self):
-        self._color_analyzer.dominant_colors = numpy.array(
-            [[0,0], [0,1], [0,2], [0,3], [0,4], [0,5], [0,6]])
-        self._color_analyzer.image_cluster_index = numpy.array(
-            [0, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 4, 4, 5, 5, 5])
+        TEST_COLORS = [[0,0], [0,1], [0,2], [0,3], [0,4], [0,5], [0,6]]
+        TEST_CLUSTER = [0, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 4, 4, 5, 5, 5]
+        EXPECTED_RESULT = [[0,2], [0,3], [0,1], [0,5], [0,4]]
+
+        self._color_analyzer.dominant_colors = numpy.array(TEST_COLORS)
+        self._color_analyzer.image_cluster_index = numpy.array(TEST_CLUSTER)
 
         _result = self._color_analyzer.get_top_5_colors()
 
-        numpy.testing.assert_array_equal(
-            [[0,2], [0,3], [0,1], [0,5], [0,4]],
-            _result)
+        numpy.testing.assert_array_equal(_result, EXPECTED_RESULT)
+
+
+if __name__ == '__main__':
+    unittest.main()
