01cc082d48aafbf1d0ace9d9166d55a7d10d3246
ferdyferdy9 <ferdyferdy9514@gmail.com>
2021-10-14 13:55:12 +0700

    Create Monte Carlo module

---
+++ test/test_monte_carlo.py
@@ -0,0 +1,19 @@
+import unittest
+import app.monte_carlo as monte_carlo
+
+
+class TestMonteCarlo(unittest.TestCase):
+    def test_generate(self):
+        result = monte_carlo.generate(0.25, 10)
+        self.assertEqual(10, len(result))
+
+    def test_success(self):
+        result = monte_carlo.test_success(0.0)
+        self.assertFalse(result)
+
+        result = monte_carlo.test_success(1.0)
+        self.assertTrue(result)
+
+
+if __name__ == '__main__':
+    unittest.main()
