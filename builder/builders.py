9e449ef580ad983873caff44782e7d2d5982b91c
ferdyferdy9 <ferdyferdy9514@gmail.com>
2021-10-14 12:25:42 +0700

    Create builders.py

---
+++ builder/builders.py
@@ -0,0 +1,9 @@
+from builder.color_analyzer_dataset_builder import *
+from builder.gann_trainer_builder import *
+
+
+class A:
+    ColorPairData = ColorPairDataBuilder()
+    ColorAnalyzerData = ColorAnalyzerDataBuilder()
+    ColorAnalyzerDataset = ColorAnalyzerDatasetBuilder()
+    GANNTrainer = GANNTrainerBuilder()
