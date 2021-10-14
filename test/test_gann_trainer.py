00fcdb01393bcb8e09938e4642448420b34357e6
ferdyferdy9 <ferdyferdy9514@gmail.com>
2021-10-14 12:26:03 +0700

    Update test_gann_trainer.py

--- test/test_gann_trainer.py
+++ test/test_gann_trainer.py
@@ -1,11 +1,12 @@
 import unittest
 from app.gann_trainer import GANNTrainer
 from app.gann import GANN
+from builder.builders import A
 
 
 class TestGANNTrainer(unittest.TestCase):
     def test_initialization(self):
-        gann_trainer = GANNTrainer(population_size=5)
+        gann_trainer = A.GANNTrainer.with_population_size(5).build()
         self.assertEqual(len(gann_trainer.population), 5)
         self.assertIsInstance(gann_trainer.population[0], GANN)
 
