1bccb1873d37727c4f7b415323366708d81c00d4
ferdyferdy9 <ferdyferdy9514@gmail.com>
2021-10-14 12:26:42 +0700

    Create GANNTrainerConfig

--- app/gann_trainer.py
+++ app/gann_trainer.py
@@ -4,19 +4,20 @@ import random
 
 
 class GANNTrainer:
-    def __init__(self, population_size=100, mutation_rate=0.1, gann_shape=[20, 16, 8, 8, 3]):
-        self.population_size = population_size
+    def __init__(self, config):
+        self.config = config
         self.population = numpy.array([])
         self.population_score = numpy.array([])
-        self.mutation_rate = mutation_rate
-        self.gann_shape = gann_shape
 
-        self.initialization()
+        if config.is_valid():
+            self.initialization()
+        else:
+            raise Exception("Configuration not valid!")
 
     def initialization(self):
         population = []
-        for _i in range(self.population_size):
-            population += [GANN(self.gann_shape)]
+        for _i in range(self.config.population_size):
+            population += [GANN(self.config.gann_shape)]
         self.population = numpy.array(population)
 
     def next_generation(self):
@@ -32,3 +33,17 @@ class GANNTrainer:
 
     def reproduction(self):
         pass
+
+
+class GANNTrainerConfig:
+    def __init__(self):
+        self.population_size = 100
+        self.mutation_rate = 0.1
+        self.gann_shape = None
+        self.dataset = None
+
+    def is_valid(self):
+        return self.gann_shape is not None \
+               and self.dataset is not None \
+               and 0.0 < self.mutation_rate <= 1.0 \
+               and 0 < self.population_size
