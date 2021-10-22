892ac63315909b48bc7867bf431e82d39ea4cc63
ferdyferdy9 <ferdyferdy9514@gmail.com>
2021-10-23 01:10:06 +0700

    Add func get_top_5_color_percentage in DominantColorAnalyzer

--- app/core/utils/dominant_color_analyzer.py
+++ app/core/utils/dominant_color_analyzer.py
@@ -5,7 +5,7 @@ from sklearn.cluster import KMeans
 
 
 class DominantColorAnalyzer:
-    def __init__(self, total_cluster=12):
+    def __init__(self, total_cluster=8):
         self.total_cluster = total_cluster
         self.image_cluster_index = None
         self.dominant_colors = None
@@ -17,12 +17,19 @@ class DominantColorAnalyzer:
 
     def analyze_image(self, image_data):
         km = KMeans(n_clusters=self.total_cluster)
-        self.image_cluster_index = km.fit_predict(image_data)
+        self.image_cluster_index = km.fit_predict(numpy.reshape(image_data, (-1, 3)))
         self.dominant_colors = km.cluster_centers_
 
     def get_top_5_colors(self):
-        sum = numpy.bincount(self.image_cluster_index)
+        cluster_sum = numpy.bincount(self.image_cluster_index)
 
-        sorted_index = numpy.argsort(sum) # return hasil index untuk sorting dari kecil ke besar
+        sorted_index = numpy.argsort(cluster_sum) # return hasil index untuk sorting dari kecil ke besar
         top_5_index = sorted_index[:-6:-1] # Ambil 5 data dari belakang
         return self.dominant_colors[top_5_index]
+
+    def get_top_5_colors_percentage(self):
+        cluster_sum = numpy.bincount(self.image_cluster_index)
+
+        sorted_cluster_sum = numpy.sort(cluster_sum)  # return hasil index untuk sorting dari kecil ke besar
+        top_5_cluster_sum = sorted_cluster_sum[:-6:-1]
+        return top_5_cluster_sum / numpy.sum(cluster_sum) # Ambil 5 data dari belakang
