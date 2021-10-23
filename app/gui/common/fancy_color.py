572c1a839d78eb8132b0943655aad5b915595be8
ferdyferdy9 <ferdyferdy9514@gmail.com>
2021-10-24 00:30:51 +0700

    Update fancy_color.py

--- app/gui/common/fancy_color.py
+++ app/gui/common/fancy_color.py
@@ -1,5 +1,6 @@
-from tkinter import ttk
 import tkinter as tk
+
+from app.core.common.color_converter import *
 
 
 class FancyColor(tk.Frame):
@@ -12,16 +13,34 @@ class FancyColor(tk.Frame):
     def _init_components(self):
         self._canvas = tk.Canvas(self, width=50, height=50)
         self._canvas.grid(row=0, column=0, rowspan=2)
-        self._canvas.create_rectangle(00, 0, 50, 50, fill='white')
+        self._update_canvas_color()
 
-        self._lbl_hex = tk.Label(self, text="HEX: {}".format("#FFFFFF"))
+        self._lbl_hex = tk.Label(self)
+        self._update_hex_text()
         self._lbl_hex.grid(row=0, column=1, sticky='w')
 
-        self._lbl_rgb = tk.Label(self, text="RGB: {}".format("255, 255, 255"))
+        self._lbl_rgb = tk.Label(self, width=16, anchor="w")
+        self._update_rgb_text()
         self._lbl_rgb.grid(row=1, column=1, sticky='w')
 
-    def set_color(self):
-        pass
+    def set_color(self, new_color):
+        self._color = new_color
+        self._update_canvas_color()
+        self._update_rgb_text()
+        self._update_hex_text()
 
     def get_color(self):
-        pass
+        return self._color
+
+    def _update_canvas_color(self):
+        hex = rgb_to_hex(self._color)
+        self._canvas.create_rectangle(0, 0, 50, 50, fill=hex)
+
+    def _update_hex_text(self):
+        hex = rgb_to_hex(self._color)
+        new_text = "HEX: {}".format(hex)
+        self._lbl_hex.config(text=new_text)
+
+    def _update_rgb_text(self):
+        new_text = "RGB: %d, %d, %d" % tuple(self._color)
+        self._lbl_rgb.config(text=new_text)
