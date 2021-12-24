30bf293af6e2d7bd1a7f1c7403660b702af3dce0
FXLJA <ja669543@gmail.com>
2021-12-24 20:01:02 +0700

    import files from mutation branch

--- app/dataset/dataset_generator.py
+++ app/dataset/dataset_generator.py
@@ -1,11 +1,11 @@
+from app.core.utils.dominant_color_analyzer import DominantColorAnalyzer
+from app.core.utils.color_analyzer_dataset import *
 import app.core.utils.color_analyzer_dataset_io as dataset_io
 
-from app.core.utils.color_analyzer_dataset import *
-from app.core.utils.dominant_color_analyzer import DominantColorAnalyzer
 
 GENERATED_FILE_NAME = "dataset.csv"
 K_MEANS_CLUSTER_TOTAL = 8
-FILE_PATH_FORMAT = "./posters/{}/{}.jpg"
+FILE_PATH_FORMAT = "./{}/{}.jpg"
 CATAGORY_AND_FOLDER = (
     (ColorAnalyzerData.HORROR, "Horror"),
     (ColorAnalyzerData.ROMANTIC, "Romance"),
@@ -17,7 +17,7 @@ def create_color_pairs(file_name):
     color_analyzer = DominantColorAnalyzer(K_MEANS_CLUSTER_TOTAL)
     color_analyzer.analyze_path(file_name)
 
-    top_5_colors = color_analyzer.get_top_5_colors() / 255.0
+    top_5_colors = color_analyzer.get_top_5_colors()/255.0
     top_5_colors_percentage = color_analyzer.get_top_5_colors_percentage()
 
     color_pairs = []
@@ -34,12 +34,12 @@ if __name__ == '__main__':
     dataset = []
     for catagory, folder_name in CATAGORY_AND_FOLDER:
         for index in range(100):
-            file_name = FILE_PATH_FORMAT.format(folder_name, index + 1)
+            file_name = FILE_PATH_FORMAT.format(folder_name, '{0:03}'.format(index+1))
             color_pairs = create_color_pairs(file_name)
             color_data = ColorAnalyzerData(color_pairs, catagory)
             dataset.append(color_data)
 
             progress += 1
-            print("Progress : {:.2f}%".format(progress / max_progress * 100))
+            print("Progress : {:.2f}%".format(progress/max_progress * 100))
 
     dataset_io.save_dataset(GENERATED_FILE_NAME, dataset)
